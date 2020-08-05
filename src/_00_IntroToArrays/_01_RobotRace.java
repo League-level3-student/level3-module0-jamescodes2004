d1dc4f810b04c026ce9ebed57ece370562396b1c
James <farewar1@gmail.com>
2020-08-04 19:55:07 -0700

    new

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -8,7 +8,25 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
+	public static void main(String[] args) {
+		Robot[] robots = new Robot[5];
 	
+		for (int i = 0; i < robots.length; i++) {
+	
+			robots[i]=new Robot();
+			robots[i].setX(100+150*i);
+			robots[i].setY(500);
+		}
+while(robots[0].getY()>0 && robots[1].getY()>0 && robots[2].getY()>0 && robots[3].getY()>0 && robots[4].getY()>0) {
+				for (int i = 0; i < robots.length; i++) {
+					
+				
+					Random rand = new Random();
+			robots[i].move(rand.nextInt(50));
+				}
+}
+		}
+	}
 		//2. create an array of 5 robots.
 
 		//3. use a for loop to initialize the robots.
@@ -27,4 +45,4 @@ public class _01_RobotRace {
 	    //9. make the robots race around a circular track.
 
 	
-}
+
